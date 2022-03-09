7934b0528954a497caa7a146ff2fe3aabeafc1f2
Jamie Hewitt <jamie.hewitt15@gmail.com>
2022-03-09 17:32:12 +0300

    Calling createNftErcWithFixedRate

--- src/migration/FixedRateExchangeMigration.ts
+++ src/migration/FixedRateExchangeMigration.ts
@@ -1,53 +1,69 @@
+import Web3 from 'web3'
 import { getAndConvertDDO } from '../DDO/convertDDO'
+import { getDDO } from '../DDO/importDDO'
 import { Contract } from 'web3-eth-contract'
 import { TransactionReceipt } from 'web3-eth'
 import { AbiItem } from 'web3-utils'
-import ERC721Template from '../artifacts/ERC721Template.json'
-import ERC20Template from '../artifacts/ERC20Template.json'
 import ERC721Factory from '../artifacts/ERC721Factory.json'
 
-export async function migratedFixedRateAsset(
-  did: string,
-  ERC721TemplateAddress: string,
-  contractInstance?: Contract
-): Promise<TransactionReceipt> {
-  const v4DDO = await getAndConvertDDO(did)
-  const ERC721TemplateContract =
-    contractInstance ||
-    new this.web3.eth.Contract(
-      ERC721Template.abi as AbiItem[],
-      ERC721TemplateAddress
+export class Migration {
+  public GASLIMIT_DEFAULT = 1000000
+  public web3: Web3
+  public startBlock: number
+
+  constructor(web3: Web3, startBlock?: number) {
+    this.web3 = web3
+    this.startBlock = startBlock || 0
+  }
+
+  public async migratedFixedRateAsset(
+    did: string,
+    ERC721FactoryAddress: string,
+    nftName: string,
+    nftSymbol: string,
+    ownerAddress: string,
+    cap: number,
+    rate: number,
+    marketFee: number,
+    publishingMarketFeeAddress: string,
+    publishingMarketTokenAddress: string,
+    fixedRateExchangeAddress: string,
+    baseTokenAddress: string,
+    contractInstance?: Contract
+  ): Promise<TransactionReceipt> {
+    const v3DDO = await getDDO(did)
+    const ERC721FactoryContract =
+      contractInstance ||
+      new this.web3.eth.Contract(
+        ERC721Factory.abi as AbiItem[],
+        ERC721FactoryAddress
+      )
+    const tx = ERC721FactoryContract.methods.createNftErcWithFixedRate(
+      {
+        name: nftName,
+        symbol: nftSymbol,
+        templateIndex: 1,
+        tokenURI: 'https://oceanprotocol.com/TEST/'
+      },
+      {
+        strings: ['ERC20WithPool', 'ERC20P'],
+        templateIndex: 1,
+        addresses: [
+          ownerAddress,
+          ownerAddress,
+          publishingMarketFeeAddress,
+          publishingMarketTokenAddress
+        ],
+        uints: [cap, 0],
+        bytess: []
+      },
+      {
+        fixedPriceAddress: fixedRateExchangeAddress,
+        addresses: [baseTokenAddress, ownerAddress, publishingMarketFeeAddress],
+        uints: [18, 18, rate, marketFee, 0]
+      }
     )
-  const tx = ERC721TemplateContract.createNftErcWithFixedRate(
-    {
-      name: '72120PBundle',
-      symbol: '72PBundle',
-      templateIndex: 1,
-      tokenURI: 'https://oceanprotocol.com/nft/'
-    },
-    {
-      strings: ['ERC20WithPool', 'ERC20P'],
-      templateIndex: 1,
-      addresses: [
-        user3.address,
-        user6.address,
-        user3.address,
-        '0x0000000000000000000000000000000000000000'
-      ],
-      uints: [cap, 0],
-      bytess: []
-    },
-    {
-      fixedPriceAddress: fixedRateExchange.address,
-      addresses: [
-        erc20TokenWithPublishFee.address,
-        user3.address,
-        user6.address,
-        ZERO_ADDRESS
-      ],
-      uints: [18, 18, rate, marketFee, 0]
-    }
-  )
-  const txReceipt = await tx.wait()
-  return txReceipt
+    const txReceipt = await tx.wait()
+    return txReceipt
+  }
 }
